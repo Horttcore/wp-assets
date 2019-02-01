b49bde1c313105297bc5223c6164710c47e80e28
PHP Package Boilerplate <hello@beyondco.de>
2019-02-01 07:15:12 +0000

    Another awesome package 📦

---
+++ src/WpAssets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Horttcore\WpAssets;
+
+class WpAssets
+{
+    // Build your next great package.
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Horttcore\WpAssets\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
